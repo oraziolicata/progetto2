32920c990acb4cc80e97ed53d7ed427f0ab942c7
Your Name <olicata@stevejobs.academy>
2024-05-30 11:05:11 +0200

    basic setup

---
+++ src/main/java/com/example/progetto2/Progetto2Application.java
@@ -0,0 +1,13 @@
+package com.example.progetto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progetto2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Progetto2Application.class, args);
+    }
+
+}
